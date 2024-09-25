b623ef388ad80603c40bd8a0e6883412010aa0b1
francjpdev <javier4prado@gmail.com>
2024-09-25 16:33:21 -0600

    seccion hero

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -33,7 +33,7 @@ export const Header = () => {
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
-          <div className="w-max max:lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 mx-lg:overflow-hidden sidebar-before max-md:px-4">
+          <div className="w-full max:lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 mx-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
