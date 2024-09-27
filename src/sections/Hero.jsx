48ce824feb5a2177df9a7270a20c4a8c87329378
francjpdev <javier4prado@gmail.com>
2024-09-27 16:43:30 -0600

    seccion hero - finalizacion

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ import { Button } from "../components/Button.jsx";
 
 export const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-52 max_lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max_lg:pb-36 max-md:pt-36 max-md:pb-32  ">
       <Element name="hero" />
       <div className="container">
         <div className="relative z-2 max-w-512 max-lg:max-w-388">
@@ -20,6 +20,12 @@ export const Hero = () => {
             <Button icon="/images/zap.svg">Try it now</Button>
           </LinkScroll>
         </div>
+
+        <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+          <img src="/images/hero.png" alt="hero" className="size-1230 max-lg:h-auto" />
+        </div>
+
+
       </div>
       <Element />
     </section>
