6a0939036947caf791419be1a0ef68c6a6bb3ff9
francjpdev <javier4prado@gmail.com>
2024-10-08 23:22:31 -0600

    seccion pricing v2

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -1,10 +1,11 @@
 import clsx from "clsx";
 import { Element } from "react-scroll";
 import { useState } from "react";
+import CountUp from "react-countup";
 import { plans } from "../constants";
 
 export const Pricing = () => {
-  const [monthly, setMontly] = useState(false);
+  const [monthly, setMonthly] = useState(false);
 
   return (
     <section>
@@ -18,13 +19,13 @@ export const Pricing = () => {
             <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
               <button
                 className={clsx("pricing-head_btn", monthly && "text-p4")}
-                onClick={() => setMontly(true)}
+                onClick={() => setMonthly(true)}
               >
                 Monthly
               </button>
               <button
                 className={clsx("pricing-head_btn", !monthly && "text-p4")}
-                onClick={() => setMontly(false)}
+                onClick={() => setMonthly(false)}
               >
                 Annual
               </button>
@@ -54,13 +55,65 @@ export const Pricing = () => {
           </div>
           {/* Pricing section */}
 
-          <div className="scroll-hidde relative z-2 -mt-12 flex items-start max-xl:overflow-auto max-xl:pt-6">
+          <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:overflow-auto max-xl:pt-6">
             {plans.map((plan, index) => (
               <div
                 key={plan.id}
                 className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
               >
-                {plan.title}
+                {index === 1 && (
+                  <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl" />
+                )}
+
+                <div
+                  className={clsx(
+                    "absolute left-0 right-0 z-2 flex items-center justify-center",
+                    index === 1 ? "-top-6" : "-top-6 xl-top-11"
+                  )}
+                >
+                  <img
+                    src={plan.logo}
+                    alt={plan.title}
+                    className={clsx(
+                      "object-contain drop-shadow-2xl",
+                      index === 1 ? "size-[120px]" : "size-[88px]"
+                    )}
+                  />
+                </div>
+
+                <div
+                  className={clsx(
+                    "relative flex flex-col items-center",
+                    index === 1 ? "pt-24" : "pt-12"
+                  )}
+                >
+                  <div
+                    className={clsx(
+                      "small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase",
+                      index === 1 ? "border-p3 text-p3" : "border-p1 text-p1"
+                    )}
+                  >
+                    {plan.title}
+                  </div>
+
+                  <div className="relative z-2 flex items-center justify-center">
+                    <div
+                      className={clsx(
+                        "h-num flex items-start",
+                        index === 1 ? "text-p3" : "text-p4"
+                      )}
+                    >
+                      $
+                      <CountUp
+                        start={plan.priceMonthly}
+                        end={monthly ? plan.priceMonthly : plan.priceYearly}
+                        duration={0.4}
+                        useEasing={false}
+                        preserveValue
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
